c65930935ba96c4ce53ad8a11a9aa9e56b4dbc06
Romain Lattion <romain.lattion@epfl.ch>
2025-05-01 15:11:34 +0200

    set SENSING/ACTUATING/SERIAL intervals and comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,56 @@
-// Cyril Monette, September 2024, modified by Romain Lattion for 6 SCD30, May 2025
-// This code is for the M5StickCPlus to read data from the SCD30 sensors at Bassenges and send it to the RPi for logging
+/* Cyril Monette, September 2024, modified by Romain Lattion for 6 SCD30, May 2025
+This code is for the M5StickCPlus to read data from the SCD30 sensors at Bassenges and send it to the RPi for logging
 
+  Recognized commands from serial:
+    - "Init"          : Resets the serial buffer and initializes sensor communication.
+    - "Get data"      : Prints the latest CO2, temperature, and humidity measurements for all sensors in the format [SCD30, sensor_nb, {CO2, temp, RH}].
+ 
+  M5StickCPlus button commands:
+    - M5 button (A) press            : Navigate through sensors 1–6 (highlight selected one)
+    - Right button (B) press         : Toggle between main display (all sensor states) and zoomed display (selected sensor)
+    - M5 button hold for 2 sec       : Cycle through display modes in main view (CO2, Temperature, Humidity)
+    - Right button hold for 5 sec    : In main mode, initialize all sensors; in zoomed mode, initialize the selected sensor
+ 
+  Sensor Mapping:
+    - 6 SCD30 sensors connected to TCA9548A I2C multiplexer:
+        • Sensor 1: Channel 0
+        • Sensor 2: Channel 1
+        • Sensor 3: Channel 2
+        • Sensor 4: Channel 3
+        • Sensor 5: Channel 4
+        • Sensor 6: Channel 5
+ 
+  Display:
+    - Main mode: Shows states of all sensors (CO2, Temp, or Humidity based on display mode), with the selected sensor highlighted in yellow. Includes button instructions at the bottom:
+        • "M5: Navigate"
+        • "Right: Select"
+        • "Hold M5 (2s): Cycle through states"
+        • "Hold Right (5s): Init. all sensors"
+    - Zoomed mode: Shows CO2, temperature, and humidity for the selected sensor, with button instructions:
+        • "M5: Navigate"
+        • "Right: Exit"
+        • "Hold M5 (2s): Cycle through states"
+        • "Hold Right (5s): Init. selected sensor"
+ 
+  Notes:
+    - Loop runs every 20ms to check buttons for responsiveness.
+    - Sensors are polled every SENSING_INTERVAL seconds (one sensor every ~SENSING_INTERVAL*1000/NUM_SENSORS ms)
+    - Display is updated every ACTUATING_INTERVAL seconds (>= SENSING_INTERVAL) after all sensors are polled.
+    - Serial output is printed every SERIAL_INTERVAL seconds (> SENSING_INTERVAL).
+
+*/
 #include "M5StickCPlus.h"
 #include "SparkFun_SCD30_Arduino_Library.h"
 
 #define NUM_SENSORS 6 // Number of sensors connected to the TCA9548A
-#define SENSING_INTERVAL 4 // Seconds
+#define SENSING_INTERVAL 8 // Seconds
+#define ACTUATING_INTERVAL SENSING_INTERVAL // Seconds, must be >= SENSING_INTERVAL
+#define SERIAL_INTERVAL 10 // Seconds, must be > SENSING_INTERVAL
 
 #define ADDRESS 0x70 // I2C address of the TCA9548A
 #define BAUD_RATE 115200
 
-//#define DEBUG
+#define DEBUG
 const static bool display = true;
 
 static SCD30 sensors[NUM_SENSORS]; // Create an array of SCD30 sensors
@@ -224,15 +264,18 @@ void tcaselect(uint8_t i) {
 }
 
 // Function to parametrise the sensors
+// Parameters: sensingInterval: the interval between measurements in seconds, between 2 and 100 seconds
 static void parametriseSensors(int number){
-    sensors[number].setMeasurementInterval(SENSING_INTERVAL);
+    sensors[number].setMeasurementInterval(SENSING_INTERVAL);//Change number of seconds between measurements: 2 to 1800 (30 minutes), stored in non-volatile memory of SCD30
     delay(200);
     int interval = sensors[number].getMeasurementInterval();
-    #ifdef DEBUG
     char msg[50];
     sprintf(msg, "Measurement Interval: %d", interval);
+    #ifdef DEBUG
     Serial.println(msg);
     #endif
+
+    //The lab is ~400m above sealevel
     sensors[number].setAltitudeCompensation(400);
     delay(200);
 }
@@ -243,9 +286,9 @@ static void init_sensor(uint8_t sensor_nb){
     bool success = sensors[sensor_nb].begin(Wire);
     displayInitStatus(sensor_nb, success);
     while(!success){
-        #ifdef DEBUG
         char error_msg[50];
         sprintf(error_msg, "Error: Could not connect to sensor %d", sensor_nb);
+        #ifdef DEBUG
         Serial.println(error_msg);
         #endif 
         delay(500);
@@ -253,9 +296,10 @@ static void init_sensor(uint8_t sensor_nb){
         success = sensors[sensor_nb].begin(Wire);
         displayInitStatus(sensor_nb, success);
     }
-    #ifdef DEBUG
+ 
     char success_msg[50];
     sprintf(success_msg, "Connected to sensor %d", sensor_nb);
+    #ifdef DEBUG
     Serial.println(success_msg);
     #endif
     parametriseSensors(sensor_nb);
@@ -298,6 +342,7 @@ static void scanI2CAddresses() {
 }
 
 // Function to format the latest measurements of the sensors to a log
+// Example of formatted log: "[SCD30, 2, {410, 23.2, 53.0}]"
 String format_log(uint8_t sensor_nb){
     String output = "[SCD30, ";
     output = output + String(sensor_nb + 1) + ", ";
@@ -332,19 +377,32 @@ void displayLatestSensorData(){
 
 // Function to get data from the sensor
 void get_data_from_sensor(int number){
-    tcaselect(number);
-    delay(100);
-    if(sensors[number].dataAvailable()){
-        co2_meas[number] = sensors[number].getCO2();
-        temp_meas[number] = sensors[number].getTemperature();
-        rh_meas[number] = sensors[number].getHumidity();
-        displayLatestSensorData();
-        #ifdef DEBUG
-        Serial.print("New data available for sensor: ");
-        Serial.println(number + 1);
-        String output = format_log(number);
-        Serial.println(output);
-        #endif
+    static unsigned long sensor_wait_start[NUM_SENSORS] = {0};
+    static bool waiting[NUM_SENSORS] = {false};
+    static uint16_t temp_co2[NUM_SENSORS] = {0};
+    static float temp_temp[NUM_SENSORS] = {0};
+    static float temp_rh[NUM_SENSORS] = {0};
+
+    if (!waiting[number]) {
+        tcaselect(number);
+        sensor_wait_start[number] = millis();
+        waiting[number] = true;
+    }
+
+    if (waiting[number] && millis() - sensor_wait_start[number] >= 100) {
+        if (sensors[number].dataAvailable()) {
+            // Store temporary data
+            temp_co2[number] = sensors[number].getCO2();
+            temp_temp[number] = sensors[number].getTemperature();
+            temp_rh[number] = sensors[number].getHumidity();
+            #ifdef DEBUG
+            Serial.print("New data polled for sensor: ");
+            Serial.println(number + 1);
+            Serial.printf("Temp CO2: %d ppm, Temp: %.1f C, RH: %.1f%%\n", 
+                         temp_co2[number], temp_temp[number], temp_rh[number]);
+            #endif
+        }
+        waiting[number] = false;
     }
 }
 
@@ -396,6 +454,16 @@ void setup() {
 
 void loop(){
     static unsigned long last_loop = millis();
+    static unsigned long last_sensor_poll = 0;
+    static unsigned long last_display_update = 0;
+    static unsigned long last_serial_update = 0;
+    static uint8_t current_sensor = 0;
+    static uint8_t sensors_polled = 0;
+    static uint16_t temp_co2[NUM_SENSORS] = {0};
+    static float temp_temp[NUM_SENSORS] = {0};
+    static float temp_rh[NUM_SENSORS] = {0};
+
+    // Run loop every 20ms for button responsiveness
     if (millis() - last_loop >= 20) {
         last_loop = millis();
 
@@ -456,9 +524,40 @@ void loop(){
             }
         }
 
-        // Process sensor data
-        for (int i = 0; i < NUM_SENSORS; i++){
-            get_data_from_sensor(i);
+        // Poll one sensor every ~SENSING_INTERVAL*1000/NUM_SENSORS ms
+        if (millis() - last_sensor_poll >= (SENSING_INTERVAL * 1000 / NUM_SENSORS)) {
+            get_data_from_sensor(current_sensor);
+            // Store data temporarily
+            if (sensors[current_sensor].dataAvailable()) {
+                temp_co2[current_sensor] = sensors[current_sensor].getCO2();
+                temp_temp[current_sensor] = sensors[current_sensor].getTemperature();
+                temp_rh[current_sensor] = sensors[current_sensor].getHumidity();
+                sensors_polled++;
+            }
+            current_sensor = (current_sensor + 1) % NUM_SENSORS;
+            last_sensor_poll = millis();
+
+            // If all sensors have been polled, update global data and display
+            if (sensors_polled >= NUM_SENSORS && millis() - last_display_update >= ACTUATING_INTERVAL * 1000) {
+                for (int i = 0; i < NUM_SENSORS; i++) {
+                    co2_meas[i] = temp_co2[i];
+                    temp_meas[i] = temp_temp[i];
+                    rh_meas[i] = temp_rh[i];
+                }
+                displayLatestSensorData();
+                sensors_polled = 0; // Reset counter
+                last_display_update = millis();
+            }
+        }
+
+        // Update Serial output every SERIAL_INTERVAL seconds
+        if (millis() - last_serial_update >= SERIAL_INTERVAL * 1000) {
+            Serial.println("Serial data output for all sensors:");
+            for (int i = 0; i < NUM_SENSORS; i++) {
+                String output = format_log(i);
+                Serial.println(output);
+            }
+            last_serial_update = millis();
         }
 
         // Handle serial input
